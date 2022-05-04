003b8715683676984cc153c03c442942f0656304
Thomas Alpert <thomasalpert@gmail.com>
2022-05-05 01:30:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ resource "openstack_compute_instance_v2" "mattermost-bots" {
       type = "ssh"
       host = self.access_ip_v4
       user = "ubuntu"
+      private_key = "${file("~/.ssh/id_rsa")}"
       # private_key = file("~/.ssh/id_rsa")
     }
     inline = [
@@ -60,7 +61,7 @@ resource "null_resource" "softwareconfig" {
     host = openstack_compute_instance_v2.mattermost-bots.access_ip_v4
     user = "ubuntu"
     port = 22
-    private_key = ~/.ssh/id_rsa
+    private_key = "${file("~/.ssh/id_rsa")}"
     
   }
 
